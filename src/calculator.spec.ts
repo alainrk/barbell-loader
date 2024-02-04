2c31d644c804c9caf5dde58f3be5848579453dbc
Alain <alain.devcs@gmail.com>
2024-02-04 09:23:09 +0100

    fix tests easy case

--- src/calculator.spec.ts
+++ src/calculator.spec.ts
@@ -20,12 +20,12 @@ const aw1 = {
 
 const cases: Array<TestCase> = [
   { bbw: 10, tw: 10, aw: aw1, exp: { left: 0, right: 0 } },
-  // { bbw: 10, tw: 20, aw: aw1, exp: { left: 5, right: 5 } },
-  // { bbw: 10, tw: 25, aw: aw1, exp: { left: 7.5, right: 7.5 } },
-  // { bbw: 10, tw: 30, aw: aw1, exp: { left: 10, right: 10 } },
-  // { bbw: 10, tw: 31, aw: aw1, exp: { left: 10.5, right: 10.5 } },
-  // { bbw: 10, tw: 32, aw: aw1, exp: { left: 11, right: 11 } },
-  // { bbw: 10, tw: 35, aw: aw1, exp: { left: 12.5, right: 12.5 } },
+  { bbw: 10, tw: 20, aw: aw1, exp: { left: 5, right: 5 } },
+  { bbw: 10, tw: 25, aw: aw1, exp: { left: 7.5, right: 7.5 } },
+  { bbw: 10, tw: 30, aw: aw1, exp: { left: 10, right: 10 } },
+  { bbw: 10, tw: 31, aw: aw1, exp: { left: 10.5, right: 10.5 } },
+  { bbw: 10, tw: 32, aw: aw1, exp: { left: 11, right: 11 } },
+  { bbw: 10, tw: 35, aw: aw1, exp: { left: 12.5, right: 12.5 } },
   // { bbw: 10, tw: 35.5, aw: aw1, exp: { left: 5, right: 5 } },
 
   // { bbw: 0.75, tw: 10, aw: aw1, exp: { left: 5, right: 5 } },
@@ -36,7 +36,9 @@ const cases: Array<TestCase> = [
 
 for (const c of cases) {
   tap.test(`Barbell: ${c.bbw}, Target: ${c.tw}`, (t) => {
-    t.equal(calculate(c.bbw, c.tw, c.aw), c.exp);
+    const res = calculate(c.bbw, c.tw, JSON.parse(JSON.stringify(c.aw)));
+    console.log(`Res: ${JSON.stringify(res)} - Exp: ${JSON.stringify(c.exp)}`);
+    t.same(res, c.exp);
     t.end();
   });
 }
